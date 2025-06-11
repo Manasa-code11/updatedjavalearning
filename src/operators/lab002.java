b8fffa5345a6db11388f748c9a65810d7550a082
Manasa-code11 <manasa.kanaparthy@gmail.com>
2025-06-11 08:29:53 +0530

    Hello lab002 created

---
+++ src/operators/lab002.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class lab002 {
+
+
+        public static void main(String[] args) {
+            int i=65;
+            int age=65;
+            age = age+10;
+            System.out.println(age);
+        }
+    }
+
+
+
