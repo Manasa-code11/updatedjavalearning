e0258e45cfafef0e5e65acfcfb7366d49aad0a58
Manasa-code11 <manasa.kanaparthy@gmail.com>
2025-06-12 08:21:19 +0530

    Hello lab003 is created

---
+++ src/ternaroryoperator/lab003.java
@@ -0,0 +1,11 @@
+package ternaroryoperator;
+
+public class lab003 {
+    public static void main(String[] args)
+    {
+        int age=10;
+        String res= (age>18)? "YES": "NO";
+        System.out.println(res);
+
+    }
+}
