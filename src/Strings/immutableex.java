51da56219355f5950e7f13a64d21b5ca69bac40c
Manasa-code11 <manasa.kanaparthy@gmail.com>
2025-06-26 08:16:50 +0530

    Hello exam2challenge01 created

---
+++ src/Strings/immutableex.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class immutableex {
+    public static void main(String[] args)
+    {
+       String name ="manasa";
+       String wish="hello";
+       name.concat("rao");
+      // System.out.println(name);
+
+        String s1 = "hello";
+        System.out.println(wish = s1);
+         s1.concat("world"); // Concat, basically add the value or merge the value in the end.
+        //System.out.println(s1);
+
+    }
+}
---
+++ src/exam01/exam02challenge01.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class exam02challenge01 {
+    public static void main(String[] args)
+    {
+        String s ="Hello";
+        s= s+"World";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.substring(5,10));
+        System.out.println(s.charAt(1));
+    }
+}
